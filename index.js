7ca7d037a2e8675fab020fbafc00e72f90a7c92d
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2024-05-06 09:16:27 -0300

    feat : Create heroes post

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.get('/heroes/:id', async (req, res) => {
     }
 });
 
+app.post('/heroes', async (req, res) => {
+    const { nome, bencaodivina, forca, resistencia, velocidade, bencaooumaldicaoo, equipamento } = req.body;
+    try {
+        const result = await pool.query('INSERT INTO herois (nome, bencaodivina, forca, resistencia, velocidade, bencaooumaldicaoo, equipamento) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', [nome, bencaodivina, forca, resistencia, velocidade, bencaooumaldicaoo, equipamento]);
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.json({ error: error.message });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
