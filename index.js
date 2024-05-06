9eac949f3ae7562814c88685e308f973b69f1095
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2024-05-06 09:13:06 -0300

    feat : get heroes by id

--- index.js
+++ index.js
@@ -16,11 +16,22 @@ app.use(express.json());
 
 app.get('/heroes', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM heroes');
+        const result = await pool.query('SELECT * FROM herois');
         res.json({
             total : result.rowCount,
             heroes : result.rows
         });
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.json({ error: error.message });
+    }
+});
+
+app.get('/heroes/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query('SELECT * FROM herois WHERE id = $1', [id]);
+        res.json(result.rows[0]);
     } catch (error) {
         console.error('Error executing query', error);
         res.json({ error: error.message });
