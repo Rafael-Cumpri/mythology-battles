0a058fe4f525ed74fbff795cbad02e759f10dbb2
Rafael Custodio Cumpri <rafaelcumpri.senai564@gmail.com>
2024-05-06 09:10:07 -0300

    feat : get all heroes route

--- server.js
+++ server.js
@@ -14,6 +14,19 @@ const pool = new Pool({
 
 app.use(express.json());
 
+app.get('/heroes', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM heroes');
+        res.json({
+            total : result.rowCount,
+            heroes : result.rows
+        });
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.json({ error: error.message });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
